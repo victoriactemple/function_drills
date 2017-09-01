041dacc4ad44bcc6ef3375c0f52205cb2a8034dd
victoriactemple <victoriactemple@gmail.com>
2017-09-01 19:05:45 -0400

    created js file for function_drills_homework

---
+++ function_drills_homework.js
@@ -0,0 +1,11 @@
+////// WRITE A FUNCTION THAT TAKES ___ AS INPUT AND RETURNS ___ AS OUTPUT //////
+
+// #1
+// Input: an array of numbers
+// Output: the sum of the numbers that were passed in
+// Edge Case: If the array is empty, return 0
+var sumOfNums = function(numsArray){
+    // Your Code Here
+  };
+
+  
